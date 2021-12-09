7024f575a213494733a47f8aaa9db803c480402f
Mohit Arora <me@aboutmohit.com>
2021-12-09 11:07:24 +0530

    init commit

---
+++ api-gateway/src/test/java/com/aboutmohit/micro/gate/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboutmohit.micro.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
